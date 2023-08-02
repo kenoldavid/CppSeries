2ea3e068e12446992bd2251f871eda432a1a17a8
Kenol David <kenoldavid.git@gmail.com>
2023-08-02 04:40:56 -0400

    Added a Log function

--- HelloWorld/HelloWorld/src/Main.cpp
+++ HelloWorld/HelloWorld/src/Main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 
+void Log(const char* message)
+{
+	std::cout << message << std::endl;
+}
+
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+	Log("Hello World!");
 	std::cin.get();
-
 }
